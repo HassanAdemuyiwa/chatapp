f3514b54a083b8c104b7ef362af3efe45d4c7988
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2020-11-20 18:12:15 +0100

    chatapp

---
+++ api/controllers/ArticlesController.js
@@ -0,0 +1,14 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    list:function(req, res){
+        res.view('list'); 
+     }
+
+};
+
